51a5d4a010012b1cf1618bc6c23dc40971f38749
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2024-08-12 18:16:20 -0300

    cambio temporal

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ErrorPage from './Components/Pages/ErrorPage/ErrorPage';
 import Home from './Components/Pages/Home/Home';
 import { LeagueProvider } from './Components/Context/LeagueContext';
 import { ThemeContext, ThemeProvider } from './Components/Context/ThemeContext';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import ProjectsPage from './Components/Pages/ProjectsPage/ProjectsPage';
 
 const router = createBrowserRouter([
@@ -24,12 +24,30 @@ const router = createBrowserRouter([
 
 const AppContent = () => {
   const { theme } = useContext(ThemeContext);
+  const [backgroundClass, setBackgroundClass] = useState();
+
   useEffect(() => {
     document.body.className = theme === 'Dark' ? 'dark-theme' : 'light-theme';
   }, [theme]);
+
+  const changeBackground = (className) => {
+    setBackgroundClass(className);
+  };
+
+  useEffect(() => {
+    document.body.classList.add(backgroundClass);
+    return () => {
+      document.body.classList.remove(backgroundClass);
+    };
+  }, [backgroundClass]);
+
   return (
     <div className={'App'}>
       <RouterProvider router={router} />
+      <button onClick={() => changeBackground('background-1')}>Background 1</button>
+      <button onClick={() => changeBackground('background-2')}>Background 2</button>
+      <button onClick={() => changeBackground('background-3')}>background 3</button>
+      <button onClick={() => changeBackground('background-4')}>background 4</button>
     </div>
   )
 }
