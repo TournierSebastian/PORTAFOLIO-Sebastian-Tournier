e95e24bddcb6d256fdf93d4f1554e03b72713800
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2024-08-14 20:02:04 -0300

    MyProyects mejoras color + responsive

--- src/Components/Pages/ProjectsPage/ProjectsPage.js
+++ src/Components/Pages/ProjectsPage/ProjectsPage.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import './ProjectsPage.css'
 import Flecha from '../../Download/Images/Flecha.png'
+import Flecha_Llena from '../../Download/Images/Flecha_Llena.png'
 import EasyFit from '../../Cards/Projects/EasyFit/EasyFit'
 const ProjectsPage = () => {
   return (
     <div className='ProjectsPage'>
       <div className='ProjectsPage_Title'>
       <a href='/' className='Icon_Return'>
-      <img src={Flecha}  className='Image_Return'> 
-      </img></a>
+      <div className='Image_Return'> </div>
+      </a>
       <h1>Mis Proyectos</h1>
       </div>
-      <div>
+      <div className='Proyects-Container'>
       <EasyFit/>
+      <EasyFit/>
+
+      <EasyFit/>
+
       </div>
     </div>
   )
