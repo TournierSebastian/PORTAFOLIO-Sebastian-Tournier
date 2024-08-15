5d7af002152d196edefc4f48978fa575ba011d0a
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2024-08-15 02:42:25 -0300

    Cambio de nombres y mejoras en responsive

--- src/Components/Cards/Projects/EasyFit/EasyFit.js
+++ src/Components/Cards/Projects/EasyFit/EasyFit.js
@@ -13,45 +13,45 @@ const EasyFit = () => {
     const description = `En este proyecto, desarrollé una página de nutrición relacionada con las calorías, donde implementé un sistema CRUD con funciones diferenciadas para usuarios y administradores. Utilicé tecnologías como React, HTML, Firebase, CSS y Bootstrap. Trabajé de manera colaborativa con un equipo bien organizado de dos personas, distribuyendo tareas para llevar a cabo el proyecto de manera efectiva`;
 
     return (
-        <div className='EasyFit_Container' onClick={handleClick}>
-            <div className='EasyFit_Info'>
+        <div className='project-container' onClick={handleClick}>
+            <div className='project-info'>
                 <h3>EasyFit</h3>
                 <p>{description.slice(0, 35)}...</p>
-                <div className='EasyFit_TechnologiesContainer'>
-                    <div className='EasyFit_Technologies'>
+                <div className='tech-stack'>
+                    <div className='tech-item'>
                         <p>React</p>
                     </div>
-                    <div className='EasyFit_Technologies'>
+                    <div className='tech-item'>
                         <p>FireBase</p>
                     </div>
-                    <div className='EasyFit_Technologies'>
+                    <div className='tech-item'>
                         <p>Bootstrap</p>
                     </div>
                 </div>
             </div>
-            <img src={Fondo} className='EasyFit_Fondo' alt='Fondo' />
+            <img src={Fondo} className='project-background' alt='Background' />
             {showMessage && (
-                <div className='message-overlay'>
-                    <div className='message-content'>
-                        <button onClick={handleClick} className='Button_EasyFit'>X</button>
+                <div className='overlay'>
+                    <div className='overlay-content'>
+                        <button onClick={handleClick} className='close-button'>X</button>
                         <h3>EasyFit</h3>
-                        <p className='description'>{description}</p>
-                        <div className='overlay_TechnologiesContainer'>
-                            <div className='overlay_Technologies'>
+                        <p className='full-description'>{description}</p>
+                        <div className='overlay-tech-stack'>
+                            <div className='overlay-tech-item'>
                                 <p>React</p>
                             </div>
-                            <div className='overlay_Technologies'>
+                            <div className='overlay-tech-item'>
                                 <p>FireBase</p>
                             </div>
-                            <div className='overlay_Technologies'>
+                            <div className='overlay-tech-item'>
                                 <p>Bootstrap</p>
                             </div>
                         </div>
-                        <div className='Github_EasyFit'>
+                        <div className='github-info'>
                             <img src={GitHubIcon} alt='GitHub Icon' />
                             <p>Git Hub</p>
                         </div>
-                        <a href='https://github.com/NekiroPLZ/easyfitv2' className='Link_EasyFit'>https://github.com/NekiroPLZ/easyfitv2</a>
+                        <a href='https://github.com/NekiroPLZ/easyfitv2' className='github-link'>https://github.com/NekiroPLZ/easyfitv2</a>
                     </div>
                 </div>
             )}
